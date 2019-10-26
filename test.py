917be8021e62e7d725cc1c36ea42172bb39fc9d9
dobrosun <mr.dobrogost02@mail.ru>
2019-10-26 15:49:18 +0300

    threads can parse and push lines to table

--- test.py
+++ test.py
@@ -1,53 +1,84 @@
 #!/usr/bin/env python3
 
 import queue
+import re
+import sqlite3
 import sys
 import subprocess
 import tempfile
 import threading as thr
-import multiprocessing as mpr
+import multiprocessing as mp
 
-class CallerThread(mpr.Process):
+class CallerThread(thr.Thread):
     def __init__(self, work_q, lock):
         super().__init__()
         self.work_q = work_q
         self.lock = lock
 
+
     def run(self):
         proc = subprocess.Popen(["find * -type f"], shell=True, stdout=subprocess.PIPE)
         filenames = proc.communicate()[0].decode().split("\n")
         
-        print("Before waiting", filenames)
+        print("Before waiting", filenames, sep=": ")
 
         th = thr.Thread(target=lambda proc: proc.wait(), args=(proc,))
-        th.start()
-
         
         for file in filenames:
-            if file == "":
+            if file == "" or file == DB_NAME:
                 continue
             self.work_q.put(file)
 
         running_workers = []
         nthreads = len(filenames) if len(filenames) < 17 else 16
-        print(nthreads)
+        nthreads = 1
         for i in range(nthreads):
             th = WorkerThread(self.lock, self.work_q)
             th.start()
 
-class WorkerThread(mpr.Process):
+class WorkerThread(thr.Thread):
+    PY_RE = [r".* = .*", r"def \w*(.*):", r"class \w*(.*):"]
+
     def __init__(self, lock, work_q):
         super().__init__()
         self.lock = lock
         self.work_q = work_q
         self._running = True
+        self.connected = False
+        self.conn = self.cursor = None
+
+    def parse_file(self, filename):
+        with open(filename, "r") as f:
+            for i, line in enumerate(f.read().split("\n")):
+                for pattern in self.PY_RE:
+                    compiled_p = re.compile(pattern)
+                    res = re.search(compiled_p, line)
+                    if not res:
+                        continue
+                    #print(i, res.group(0), sep="    ")
+                    self.push_to_db((filename, i, res.group(0)))
+
+    def push_to_db(self, values):
+        def _create_conn():
+            self.conn = sqlite3.connect(DB_NAME)
+            self.cursor = self.conn.cursor()
+
+        if not self.connected:
+            _create_conn()
+            self.connected = True
+        sql = """INSERT INTO defs VALUES (?, ?, ?)"""
+        #print(*values, sep="  ")
+        self.cursor.executemany(sql, [values])
+        with self.lock:
+            self.conn.commit()
 
     def run(self):
         while self._running:
             try:
                 task = self.work_q.get_nowait()
                 
-                print("Semo")
+                #print("Semo")
+                self.parse_file(task)
                 # parse file and push to with self.lock: database
 
                 #self.work_q.task_done()
@@ -58,24 +89,58 @@ class WorkerThread(mpr.Process):
     def terminate(self):
         self._running = False
 
+DB_NAME = "w.db"
+
+def print_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    sql = """SELECT * FROM defs"""
+    for row in cursor.execute(sql):
+        print(row)
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    def del_all_in_db(conn, cursor):
+        try:
+            sql = """DROP TABLE defs"""
+            cursor.execute(sql)
+        except:
+            pass
+
+    del_all_in_db(conn, cursor)
+
+    sql = """CREATE TABLE defs
+                (filename text, line int, row text)"""
+    try:
+        cursor.execute(sql)
+    except:
+        pass
+
+
 def main():
     #tmp_db = tempfile.NamedTemporaryFile(suffix=".db")
     #f = open("w.db")
-    lock = mpr.Lock()
-    work_q = mpr.Queue()
+    lock = mp.Lock()
+    work_q = mp.Queue()
 
+    conn = sqlite3.connect(DB_NAME)
 
     caller = CallerThread(work_q, lock)
     caller.start()
 
-
     #tmp_db.close()
 
 if __name__ == "__main__":
-    prc = mpr.Process(target=main, args=())
+    init_db()
+    prc = thr.Thread(target=main, args=())
     prc.start()
 
     a = 1
     for i in range(10):
         a += a * a
-        print(a)
+
+    prc.join()
+    print_db()
+    print("END")
